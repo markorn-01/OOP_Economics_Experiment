9a18e09153a3df852aafa08141d319c3d59b175f
Minh <minh17301@gmail.com>
2026-02-12 20:36:17 +0100

    feat: initial creation of Payment page

--- myproject/mysurvey/__init__.py
+++ myproject/mysurvey/__init__.py
@@ -1,7 +1,7 @@
 from symtable import Class
 
+from charset_normalizer import models
 from otree.api import *
-
 
 doc = """
 Your app description
@@ -23,6 +23,7 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    # Section 1:
     # Q1: Age group
     age_group = models.StringField(
         label='Q1: What is your age group?',
@@ -57,6 +58,9 @@ class Player(BasePlayer):
         choices=['Urban', 'Suburban', 'Rural']
     )
 
+    # Section 2:
+    payment_methods = models.LongStringField(blank=True)
+
 # PAGES
 class Introduction(Page):
     pass
@@ -65,10 +69,22 @@ class Demographics(Page):
     form_model = 'player'
     form_fields = ['age_group', 'gender', 'income', 'education', 'location']
 
+class Payment(Page):
+    form_model = 'player'
+    form_fields = ['payment_methods']
+                   # 'factors',
+                   # 'frequency',
+                   # 'provider',
+                   # 'service_reason',
+                   # 'is_abandoned',
+                   # 'missed_bnpl',
+                   # 'product_type',
+                   # 'statement_rate']
+
 class Results(Page):
     pass
 
 class Test(Page):
     pass
 
-page_sequence = [Introduction, Demographics, Test]
+page_sequence = [Introduction, Demographics, Payment,  Test]
